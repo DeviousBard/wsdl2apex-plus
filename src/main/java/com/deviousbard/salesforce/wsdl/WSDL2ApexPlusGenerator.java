8abce7d317037f116ccec0ec955a7bd8e18e97db
DeviousBard <github@deviousbard.com>
2016-12-20 13:55:38 -0500

    Version 0.4

--- src/main/java/com/deviousbard/salesforce/wsdl/WSDL2ApexPlusGenerator.java
+++ src/main/java/com/deviousbard/salesforce/wsdl/WSDL2ApexPlusGenerator.java
@@ -1,38 +1,101 @@
 package com.deviousbard.salesforce.wsdl;
 
-import com.predic8.schema.*;
+import com.predic8.schema.Schema;
+import com.predic8.schema.SchemaParser;
 import com.predic8.wsdl.Definitions;
 import com.predic8.wsdl.WSDLParser;
-import groovy.xml.QName;
 import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.VelocityEngine;
 import org.apache.velocity.runtime.RuntimeConstants;
 import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
 
+import java.io.BufferedReader;
 import java.io.FileWriter;
-import java.util.HashSet;
-import java.util.Set;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class WSDL2ApexPlusGenerator {
 
-    private VelocityEngine ve = new VelocityEngine();
+    private Map<String, Definitions> wsdlMap = new HashMap<>();
+    private Map<String, Schema> schemaMap = new HashMap<>();
 
+    private VelocityEngine ve = new VelocityEngine();
     private Template apexSchemaTemplate;
     private Template apexWebServiceTemplate;
-    private Set<String> parsedSchemaSet = new HashSet<>();
+    private String clientCertName;
+    private boolean wsSecurity;
+    private boolean environmentSpecificEndPoint;
+    private String timeout;
 
-    private void generateApexClasses(String wsdlFile) {
+    private void generateApex(String wsdlFile) {
         initializeTemplateEngine();
-        parseWSDL(wsdlFile);
+        parseWsdlList(Collections.singletonList(wsdlFile));
+        askForClassNames();
+        for (String key : schemaMap.keySet()) {
+            System.out.print("Schema:\n" + key + " - " + schemaMap.get(key) + "\n");
+            SchemaDefinition sd = new SchemaDefinition(schemaMap.get(key));
+            if (wsdlMap.get(key) == null) {
+                this.applyApexSchemaTemplate(sd);
+            }
+            for (SimpleTypeDefinition std : sd.getSimpleTypes().values()) {
+                System.out.print("   Simple Type: " + std.toString() + "\n");
+            }
+            for (ComplexTypeDefinition ctd: sd.getComplexTypes().values()) {
+                System.out.print("   Complex Type: " + ctd.toString() + "\n");
+            }
+            for (ElementDefinition ed: sd.getElements().values()) {
+                System.out.print("   Element: " + ed.toString() + "\n");
+            }
+        }
+        for (String key : wsdlMap.keySet()) {
+            System.out.print("WSDL Definition:\n" + key + " - " + wsdlMap.get(key) + "\n\n");
+            SchemaDefinition sd = new SchemaDefinition(schemaMap.get(key));
+            WsdlDefinition wd = new WsdlDefinition(wsdlMap.get(key), clientCertName, wsSecurity, environmentSpecificEndPoint, timeout);
+            this.applyApexWebServiceTemplate(sd, wd);
+        }
+    }
+
+    private void initializeTemplateEngine() {
+        new ClasspathResourceLoader();
+        ve.setProperty(RuntimeConstants.RESOURCE_LOADER, "classpath");
+        ve.setProperty("classpath.resource.loader.class", ClasspathResourceLoader.class.getName());
+        ve.init();
+        apexSchemaTemplate = ve.getTemplate("templates/apex-schema.vm");
+        apexWebServiceTemplate = ve.getTemplate("templates/apex-web-service.vm");
+    }
+
+    private void applyApexWebServiceTemplate(SchemaDefinition sd, WsdlDefinition wd) {
+        VelocityContext context = new VelocityContext();
+        context.put("schema", sd);
+        context.put("wsdl", wd);
+        context.put("ApexUtility", ApexUtility.class);
+        System.out.println("wd: " + wd);
+        FileWriter fw = null;
+        try {
+            fw = new FileWriter("/tmp/" + ApexUtility.getApexClassFromNamespace(wd.getNamespace()) + ".cls");
+            apexWebServiceTemplate.merge(context, fw);
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        } finally {
+            try {
+                if (fw != null) {
+                    fw.close();
+                }
+            } catch (Exception e) {
+                // Intentionally ignored
+            }
+        }
     }
 
     private void applyApexSchemaTemplate(SchemaDefinition sd) {
         VelocityContext context = new VelocityContext();
         context.put("schema", sd);
+        context.put("ApexUtility", ApexUtility.class);
         FileWriter fw = null;
         try {
-            fw = new FileWriter("/tmp/" + sd.getName() + ".cls");
+            fw = new FileWriter("/tmp/" + ApexUtility.getApexClassFromNamespace(sd.getNamespace()) + ".cls");
             apexSchemaTemplate.merge(context, fw);
         } catch (Exception e) {
 
@@ -48,136 +111,98 @@ public class WSDL2ApexPlusGenerator {
         }
     }
 
-    private void parseWSDL(String wsdlFile) {
-        WSDLParser wsdlParser = new WSDLParser();
-        Definitions defs = wsdlParser.parse(wsdlFile);
-        String baseDir = (String) defs.getBaseDir();
-        //System.out.println("baseDir: " + baseDir);
-        for (Schema schema : defs.getSchemas()) {
-            parseSchema(schema, baseDir);
-        }
-    }
-
-    private SchemaDefinition parseSchema(Schema schema, String baseDir) {
-        SchemaDefinition sd = new SchemaDefinition(schema);
-        String schemaLocation = schema.getSchemaLocation();
-        if (schemaLocation != null && !schemaLocation.equals("") && !parsedSchemaSet.contains(schemaLocation)) {
-            String schemaFileName = baseDir + schemaLocation;
-            System.out.println("schemaFileName: " + schemaFileName);
-            parsedSchemaSet.add(schemaFileName);
-            String schemaName = schemaLocation.split("\\.xsd")[0] + "_XSD";
-            //System.out.println("schemaName:  " + schemaName);
-            sd.setNamespace(schema.getTargetNamespace());
-            SchemaParser schemaParser = new SchemaParser();
-            Schema parsedSchema = schemaParser.parse(schemaFileName);
-            for (Import schemaImport : parsedSchema.getImports()) {
-                SchemaDefinition importedSchema = parseSchema(new SchemaParser().parse(schemaFileName), baseDir);
-                sd.addImport(importedSchema);
+    private void askForClassNames() {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            for (String key : wsdlMap.keySet()) {
+                Definitions defs = wsdlMap.get(key);
+                String fileName = defs.getName();
+                String defaultClassName = fileName.replace('.', '_') + "_WS";
+                System.out.print("Web Service Class Name for Target Namespace \"" + defs.getTargetNamespace() + "\" [" + defaultClassName + "]: ");
+                String className = br.readLine();
+                if (className == null || className.trim().equals("")) {
+                    className = defaultClassName;
+                }
+                if (defs.getTargetNamespace() != null && !(defs.getTargetNamespace().equals(""))) {
+                    ApexUtility.addApexClassName(defs.getTargetNamespace(), className);
+                }
             }
-            for (ComplexType ct : schema.getComplexTypes()) {
-                ComplexTypeDefinition td = new ComplexTypeDefinition(ct, sd);
-                td.setName(ct.getName());
-                td.setNamespace(ct.getNamespaceUri());
-                sd.addComplexType(td);
 
-                for (SchemaComponent sc : ((ModelGroup) ct.getModel()).getParticles()) {
-                    if (sc.getClass().getSimpleName().equals("Element")) {
-                        Element el = (Element) sc;
-                        System.out.println("el: " + el);
-                        ElementDefinition ed = new ElementDefinition(el, sd);
-                        ed.setName(el.getName());
-                        ed.setElementNamespace(el.getNamespaceUri());
-                        QName qualifiedType = el.getType();
-                        if (qualifiedType != null) {
-                            ed.setType(qualifiedType.getLocalPart());
-                            ed.setTypeNamespace(qualifiedType.getNamespaceURI());
-                        } else if (el.getEmbeddedType() != null && el.getEmbeddedType() instanceof ComplexType) {
-                            ComplexType embeddedType = (ComplexType)el.getEmbeddedType();
-                            if (embeddedType.getSequence() != null) {
-                            }
-                        }
-                        ed.setMinOccurs(el.getMinOccurs() == null || el.getMinOccurs().equals("") ? 0 : Integer.parseInt(el.getMinOccurs()));
-                        ed.setMaxOccurs(el.getMaxOccurs() == null || el.getMaxOccurs().equals("") ? 0 : el.getMaxOccurs().equals("unbounded") ? -1 : Integer.parseInt(el.getMaxOccurs()));
-//                        System.out.println("element name: " + el.getName() + " - minOccurs: " + el.getMinOccurs() + "   maxOccurs: " + el.getMaxOccurs());
-                        ed.setRequired(ed.getMinOccurs() >= 1);
-                        ed.setNilable(el.isNillable());
-                        td.addElement(ed);
+            for (String key : schemaMap.keySet()) {
+                if (wsdlMap.get(key) == null) {
+                    Schema schema = schemaMap.get(key);
+                    String fileName = schema.getSchemaLocation();
+                    int index = (fileName.lastIndexOf('.') == -1 ? fileName.length() : fileName.lastIndexOf('.'));
+                    String defaultClassName = fileName.substring(fileName.lastIndexOf("/") + 1, index) + "_XSD";
+                    System.out.print("Schema Class Name for Target Namespace \"" + schema.getTargetNamespace() + "\" [" + defaultClassName + "]: ");
+                    String className = br.readLine();
+                    if (className == null || className.trim().equals("")) {
+                        className = defaultClassName;
+                    }
+                    if (schema.getTargetNamespace() != null && !(schema.getTargetNamespace().equals(""))) {
+                        ApexUtility.addApexClassName(schema.getTargetNamespace(), className);
                     }
                 }
             }
+            System.out.print("Client Certificate Name []: ");
+            this.clientCertName = br.readLine();
 
-            for (SimpleType st : schema.getSimpleTypes()) {
-                SimpleTypeDefinition std = new SimpleTypeDefinition(st, sd);
-                std.setName(st.getName());
-                QName baseQualifiedName = st.getRestriction().getBase();
-                std.setBase(baseQualifiedName.getLocalPart());
-                std.setNamespace(baseQualifiedName.getNamespaceURI());
-                if (st.getRestriction().hasEnumerationFacet()) {
-                    std.addEnumerationList(st.getRestriction().getEnumerationFacets());
-                }
-                if (st.getRestriction().getMinLengthFacet() != null) {
-                    std.setMinLength(Integer.parseInt(st.getRestriction().getMinLengthFacet().getValue()));
-                }
-                if (st.getRestriction().getMaxLengthFacet() != null) {
-                    std.setMaxLength(Integer.parseInt(st.getRestriction().getMaxLengthFacet().getValue()));
-                }
-                sd.addSimpleType(std);
-            }
-            applyApexSchemaTemplate(sd);
+            System.out.print("Use WS-Security (Y/N) [N]: ");
+            String wsSecurityAnswer = br.readLine();
+            this.wsSecurity = (wsSecurityAnswer != null && wsSecurityAnswer.toLowerCase().equals("y"));
+
+            System.out.print("Use Environment Specific End-Points (Y/N) [N]: ");
+            String environmentSpecificEndPointAnswer = br.readLine();
+            this.environmentSpecificEndPoint = (environmentSpecificEndPointAnswer != null && environmentSpecificEndPointAnswer.toLowerCase().equals("y"));
+
+            System.out.print("Web Service Timeout (in ms) (1 - 120000) [10000]: ");
+            String timeoutAnswer = br.readLine();
+            this.timeout = (timeoutAnswer == null || timeoutAnswer.trim().equals("") ? "10000" : "" + Integer.parseInt(timeoutAnswer));
+
+        } catch (Exception e) {
+            System.err.print("Exception: " + e.getMessage());
+            e.printStackTrace(System.err);
         }
-        return sd;
-
-
-//        sd.setName("RatingSystemV10_XSD");
-//        TypeDefinition td = new TypeDefinition();
-//        td.setName("AgeBandRateCountsType");
-//        td.setNamespace("http://capbluecross.com/schema/application/RatingSystem/v1.0");
-//        sd.addComplexType(td);
-//        ElementDefinition ed = new ElementDefinition();
-//        ed.setName("ageBandRateCount");
-//        ed.setType("AgeBandRateCountType");
-//        ed.setNamespace("http://capbluecross.com/schema/application/RatingSystem/v1.0");
-//        ed.setRequired("true");
-//        td.addElement(ed);
-//        td = new TypeDefinition();
-//        td.setName("AgeBandRateCountType");
-//        td.setNamespace("http://capbluecross.com/schema/application/RatingSystem/v1.0");
-//        sd.addComplexType(td);
-//        ed = new ElementDefinition();
-//        ed.setName("age");
-//        ed.setRequired("true");
-//        ed.setNamespace("http://capbluecross.com/schema/application/RatingSystem/v1.0");
-//        ed.setType("xsd:string");
-//        ed.setPrimitive(true);
-//        td.addElement(ed);
-//        ed = new ElementDefinition();
-//        ed.setName("count");
-//        ed.setRequired("true");
-//        ed.setNamespace("http://capbluecross.com/schema/application/RatingSystem/v1.0");
-//        ed.setType("xsd:string");
-//        ed.setPrimitive(true);
-//        td.addElement(ed);
-//        ed = new ElementDefinition();
-//        ed.setName("rate");
-//        ed.setRequired("true");
-//        ed.setNamespace("http://capbluecross.com/schema/application/RatingSystem/v1.0");
-//        ed.setType("xsd:string");
-//        td.addElement(ed);
-//        ed.setPrimitive(true);
-//        applyApexSchemaTemplate(sd);
     }
 
-    private void initializeTemplateEngine() {
-        new ClasspathResourceLoader();
-        ve.setProperty(RuntimeConstants.RESOURCE_LOADER, "classpath");
-        ve.setProperty("classpath.resource.loader.class", ClasspathResourceLoader.class.getName());
-        ve.init();
-        apexSchemaTemplate = ve.getTemplate("templates/apex-schema_OLD.vm");
-        apexWebServiceTemplate = ve.getTemplate("templates/apex-web-service.vm");
+    private void parseWsdlList(List<String> wsdlFileList) {
+        WSDLParser wsdlParser = new WSDLParser();
+        for (String wsdlFile : wsdlFileList) {
+            Definitions defs = wsdlParser.parse(wsdlFile);
+            String baseDir = (String) defs.getBaseDir();
+            for (Schema localSchema : defs.getLocalSchemas()) {
+                schemaMap.put(localSchema.getTargetNamespace(), localSchema);
+            }
+            parseSchemaList(defs.getLocalSchemas(), baseDir);
+            if (!(wsdlMap.keySet().contains(defs.getTargetNamespace()))) {
+                wsdlMap.put(defs.getTargetNamespace(), defs);
+                parseSchemaList(defs.getSchemas(), baseDir);
+                if (defs.getImports() != null) {
+                    List<String> importWsdlFileList = new ArrayList<>();
+                    for (com.predic8.wsdl.Import wsdlRef : defs.getImports()) {
+                        importWsdlFileList.add(baseDir + wsdlRef.getLocation());
+                        parseWsdlList(importWsdlFileList);
+                    }
+                }
+            }
+        }
+    }
+
+    private void parseSchemaList(List<Schema> schemas, String baseDir) {
+        for (Schema schemaRef : schemas) {
+            SchemaParser schemaParser = new SchemaParser();
+            if (schemaRef.getSchemaLocation() != null && !(schemaRef.getSchemaLocation().equals("")) && !(schemaMap.keySet().contains(schemaRef.getTargetNamespace()))) {
+                Schema schema = schemaParser.parse(baseDir + schemaRef.getSchemaLocation());
+                schemaMap.put(schemaRef.getTargetNamespace(), schema);
+                if (schema.getImportedSchemas() != null) {
+                    parseSchemaList(schema.getImportedSchemas(), baseDir);
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
-        WSDL2ApexPlusGenerator gen = new WSDL2ApexPlusGenerator();
-        gen.generateApexClasses(args[0]);
+        WSDL2ApexPlusGenerator generator = new WSDL2ApexPlusGenerator();
+        generator.generateApex(args[0]);
     }
 }
